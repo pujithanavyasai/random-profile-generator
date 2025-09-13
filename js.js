a7378d225c135b412e5c5464b05247ffecff6ee5
Pujitha Navya Sai <navyasaimudunuri@gmail.com>
2025-09-13 16:03:02 +0530

    updated

--- js.js
+++ js.js
@@ -9,44 +9,42 @@ const lower = document.getElementById('lower');
 skeletonCont.style.display = "none";
 realCont.style.display = "none";
 
-
-function gettingRandom(){
+function gettingRandom() {
     upper.innerText = "";
     mid.innerText = "";
     lower.innerText = "";
     generateRequest.style.display = "none";
-    skeletonCont.style.display = "block";
+    skeletonCont.style.display = "flex";
     realCont.style.display = "none";
-    setTimeout(()=>{
+    setTimeout(() => {
         skeletonCont.style.display = "none";
-        realCont.style.display = "block";
+        realCont.style.display = "flex";
         randomPerson();
-    },2000)
-
+    }, 2000)
 }
-async function randomPerson(){
-    
+
+async function randomPerson() {
     let response = await fetch("https://randomuser.me/api/");
     let obj = await response.json();
     obj = obj.results[0]
 
     upper.innerHTML = `
-    <img src="${obj.picture.large}" class="h-36 w-36 rounded-full"/>
+    <img src="${obj.picture.large}" alt="Profile Picture" />
     <div class="pCont">
-        <div id="name" class="text-[150%] font-semibold">${obj.name.title}. ${obj.name.first} ${obj.name.last}</div>
-        <div id="email" class="text-[110%] text-[#5a5555]">${obj.email}</div>
+        <div id="name" class="name">${obj.name.title}. ${obj.name.first} ${obj.name.last}</div>
+        <div id="email" class="email">${obj.email}</div>
     </div>
     `;
     mid.innerHTML = `
-    <div id="age" class="absolute top-[55%] text-[#5a5555]" ><span>Gender</span>: ${obj.gender}, <span>DOB</span>: ${new Date(obj.dob.date).toLocaleDateString("en-US",{day:"numeric",month:"long",year:"numeric"})}, <span>Age</span>: ${obj.dob.age}</div>
-    <div id="username" class="absolute top-[75%] text-[#5a5555]" ><span>Username</span>: ${obj.login.username}</div>
-    <div id="Regdate" class="absolute top-[95%] text-[#5a5555]" ><span>Registered On</span>: ${obj.registered.date.slice(0,10)}</div>
-    <div id="Add" class="absolute top-[115%] text-[#5a5555]" ><span>Address</span>: ${obj.location.street.number}-${obj.location.street.name}, ${obj.location.city}, ${obj.location.state}, ${obj.location.country} - ${obj.location.postcode}</div>
+    <div id="age" class="info"><span>Gender</span>: ${obj.gender}, <span>DOB</span>: ${new Date(obj.dob.date).toLocaleDateString("en-US",{day:"numeric",month:"long",year:"numeric"})}, <span>Age</span>: ${obj.dob.age}</div>
+    <div id="username" class="info"><span>Username</span>: ${obj.login.username}</div>
+    <div id="Regdate" class="info"><span>Registered On</span>: ${obj.registered.date.slice(0,10)}</div>
+    <div id="Add" class="info"><span>Address</span>: ${obj.location.street.number}-${obj.location.street.name}, ${obj.location.city}, ${obj.location.state}, ${obj.location.country} - ${obj.location.postcode}</div>
     `;
     lower.innerHTML = `
-    <div class="absolute top-[25%]"><span>Contact Number</span></div>
-    <div id="pNO" class="absolute top-[40%] text-[#5a5555]">${obj.phone} / ${obj.cell}</div>
+    <div class="label"><span>Contact Number</span></div>
+    <div id="pNO" class="info">${obj.phone} / ${obj.cell}</div>
     `;
-
 }
+
 button.onclick = gettingRandom;
